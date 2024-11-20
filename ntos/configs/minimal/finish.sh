323fe3c8bfefe8920ccb2da82a6f9e5f14b1c891
Daan <dselen@nerthus.nl>
2024-11-20 14:59:55 +0100

    Fixed credcon path in finish.sh

--- ntos/configs/minimal/finish.sh
+++ ntos/configs/minimal/finish.sh
@@ -18,7 +18,7 @@ read -r new_hostname
 echo -e '\nCustomizing user environment...'
 
 curl "$web_address"/"${rdp_name}".rdp > /home/"${currentUser}"/Templates/remote-connection.rdp
-curl "$web_address"/credcon.sh > /home/"${currentUser}"/Templates/credcon.sh
+curl "$web_address"/credcon/credcon.sh > /home/"${currentUser}"/Templates/credcon.sh
 
 # Download the file to /home/${currentUser}/Templates (runs as the normal user)
 wget "${web_address}"/Panel-Profile.tar.bz2 -P /home/"${currentUser}"/Templates
