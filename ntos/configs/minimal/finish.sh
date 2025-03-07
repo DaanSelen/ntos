6369bf32e7237e36d875d405b2c993ae5f995d13
Daan Selen <dselen@nerthus.nl>
2025-03-07 08:47:03 +0100

    Testing spreading out of logic in minimal installation.

--- ntos/configs/minimal/finish.sh
+++ ntos/configs/minimal/finish.sh
@@ -13,7 +13,12 @@ if [ ! -f "/etc/setup_done" ]; then
     su root -c "bash -c '
     sed -i \"/^deb cdrom:/s/^/#/\" /etc/apt/sources.list &&
     echo \"deb http://ftp.de.debian.org/debian bookworm-backports main non-free non-free-firmware\" | tee /etc/apt/sources.list.d/debian-backports.list &&
-    apt-get remove \"\$(apt list --installed 'linux-image-*' 2>/dev/null | awk -F '[/ ]' '/linux-image-[0-9]/{print \$1, \$3}' | sort -k2V | head -n1 | awk '{print \$1}')\" &&
+
+    OLD_KERNEL=\$(apt list --installed 'linux-image-*' 2>/dev/null | awk -F '[/ ]' '/linux-image-[0-9]/{print \$1, \$3}' | sort -k2V | head -n1 | awk '{print \$1}')
+    if [ -n \"\$OLD_KERNEL\" ]; then
+        apt-get remove -y \"\$OLD_KERNEL\"
+    fi &&
+
     apt-get update &&
     apt-get install -y cups curl dbus-x11 network-manager-gnome plymouth-themes sane sane-utils system-config-printer \
         xfce4 xfce4-goodies xfce4-panel-profiles xfce4-power-manager xsane yad &&
@@ -21,12 +26,14 @@ if [ ! -f "/etc/setup_done" ]; then
     apt-get remove -y firefox-esr &&
     apt-get autoremove -y &&
     apt-get clean &&
+
     echo \"Unconfigured-NTOS\" > /etc/hostname &&
     sed -i \"s/127.0.1.1.*/127.0.1.1       Unconfigured-NTOS/\" /etc/hosts &&
     sed -i \"s/quiet/quiet loglevel=3 splash i915.modeset=1/\" /etc/default/grub &&
     sed -i \"s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/\" /etc/default/grub &&
     sed -i \"s/^#\(SystemMaxUse=\).*/\150M/\" /etc/systemd/journald.conf &&
     sed -i \"s/^#autologin-user=/autologin-user=user/\" /etc/lightdm/lightdm.conf &&
+
     mkdir -p /opt/ntos && chown user:user /opt/ntos &&
     grub-mkconfig -o /boot/grub/grub.cfg'"
 
