f0b54fbe933de4ff58b389415d3201c524f4a553
Daan Selen <dselen@nerthus.nl>
2025-03-11 14:01:23 +0100

    Testing out different sequence.

--- ntos/configs/minimal/finish.sh
+++ ntos/configs/minimal/finish.sh
@@ -27,11 +27,15 @@ if [ ! -f "/etc/setup_done" ]; then
 
     apt-get clean &&
     apt-get update &&
+
+    apt-get install -y -t bookworm-backports \
+        grub-common linux-image-amd64 linux-headers-amd64 freerdp3-x11 firmware-intel-graphics firmware-realtek &&
+    apt-get clean -y &&
+    apt-get autoremove -y &&
+
     apt-get install -y cups curl dbus-x11 network-manager-gnome plymouth-themes sane sane-utils system-config-printer \
         unzip xfce4 xfce4-goodies xfce4-panel-profiles xfce4-power-manager xsane yad &&
     apt-get clean -y &&
-    apt-get install -y -t bookworm-backports \
-        grub-common linux-image-amd64 linux-headers-amd64 freerdp3-x11 firmware-intel-graphics firmware-realtek &&
     apt-get autoremove -y &&
 
     echo \"Unconfigured-NTOS\" > /etc/hostname &&
