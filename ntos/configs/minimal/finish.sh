c813a4d7e1db4958bc76d11453ba749d79fde983
Daan Selen <dselen@nerthus.nl>
2025-03-13 10:15:12 +0100

    Removed grub from backports install.

--- ntos/configs/minimal/finish.sh
+++ ntos/configs/minimal/finish.sh
@@ -35,7 +35,7 @@ if [ ! -f "/etc/setup_done" ]; then
 
     rm -rf /var/cache/* &&
     DEBIAN_FRONTEND=noninteractive apt-get install -y -t bookworm-backports \
-        grub-common freerdp3-x11 firmware-intel-graphics firmware-realtek linux-image-amd64 linux-headers-amd64 &&
+        freerdp3-x11 firmware-intel-graphics firmware-realtek linux-image-amd64 linux-headers-amd64 &&
     apt-get clean -y &&
     apt-get autoremove -y &&
     rm -rf /var/cache/* &&
